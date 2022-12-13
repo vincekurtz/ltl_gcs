b3b7ccc4445b4950708dfc995fdf889cda77523f
Vince Kurtz <vjkurtz@gmail.com>
2022-12-12 19:41:18 -0500

    initial pass at iris

--- examples/robot_arm.py
+++ examples/robot_arm.py
@@ -1,11 +1,19 @@
 from pydrake.all import *
 import numpy as np
+import time
+import pickle
 
 ##
 #
-# A temporal logic motion planning example for a high-DoF robot arm
+# A temporal logic motion planning example for a high-DoF robot arm. The robot
+# arm must press a button before reaching through a doorway. 
 #
 ##
+
+# Several important configurations, could be defined via inverse dynamics
+q0 = np.array([0.0, -0.3, 0, -2.6, 0, 2.3, 0.8])
+q_button = np.array([-2.6, 0.9, 0, -1.3, 1.8, 1.9, 0.8])
+q_door = np.array([-0.6, 1.3, -1.0, -1.0, 0, 2.3, 0.8])
 
 # Create a MultibodyPlant model of the system
 builder = DiagramBuilder()
@@ -27,10 +35,45 @@ DrakeVisualizer().AddToBuilder(builder=builder, scene_graph=scene_graph,
 diagram = builder.Build()
 diagram_context = diagram.CreateDefaultContext()
 
+# Construct convex sets using IRIS. This can be quite slow, so we do it offline
+# and save the results. 
+perform_iris = False
+if perform_iris:
+    seeds = {"q0":q0, "q_button":q_button, "q_door":q_door}
+    for name, configuration in seeds.items():
+        plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
+        plant.SetPositions(plant_context, configuration)
+
+        iris_options = IrisOptions()
+        iris_options.require_sample_point_is_contained = True
+        iris_options.iteration_limit = 1
+        iris_options.termination_threshold = 2e-2
+        iris_options.relative_termination_threshold = 2e-2
+        iris_options.num_collision_infeasible_samples = 1
+
+        start_time = time.time()
+        hpoly = IrisInConfigurationSpace(plant, plant_context, iris_options)
+
+        print(f"Generated a collision-free polytope around {name} with {len(hpoly.b())} faces in {time.time()-start_time} seconds")
+
+        with open(f"examples/iris_region_{name}.pkl", "wb") as f:
+            pickle.dump(hpoly,f)
+
+with open("examples/iris_region_q0.pkl", "rb") as f:
+    q0_region = pickle.load(f)
+with open("examples/iris_region_q_button.pkl", "rb") as f:
+    q_button_region = pickle.load(f)
+with open("examples/iris_region_q_door.pkl", "rb") as f:
+    q_door_region = pickle.load(f)
+
+print(q0_region.Intersection(q_button_region).IsEmpty())
+print(q0_region.Intersection(q_door_region).IsEmpty())
+
+
+
 # Simulate the system
 plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
-q0 = [0.0, -0.3, 0, -2.6, 0, 2.3, 0.8, 0.05, 0.05]
-plant.SetPositions(plant_context, q0)
+plant.SetPositions(plant_context, (q0 + q_door) /2)
 
 simulator = Simulator(diagram, diagram_context)
 simulator.set_target_realtime_rate(1.0)
