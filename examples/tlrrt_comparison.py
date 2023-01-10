f57b16a50511f6a91fecb996420c35e74db896ca
Vince Kurtz <vjkurtz@gmail.com>
2023-01-10 13:19:26 -0500

    clean up tlrrt comparison

--- examples/tlrrt_comparison.py
+++ examples/tlrrt_comparison.py
@@ -47,70 +47,48 @@ dfa_start_time = time.time()
 dfa = DeterministicFiniteAutomaton(spec)
 dfa_time = time.time() - dfa_start_time
 
-# Record solve times over several trials with different initial conditions
-np.random.seed(0)
-N = 100
-solve_times = []
+# Take the product of the DFA and the transition system to produce a graph of
+# convex sets
+print("Constructing GCS")
+start_point = [0.8, 0.1]
+order = 1
+continuity = 0
+product_start_time = time.time()
+bgcs = ts.Product(dfa, start_point, order, continuity)
+product_time = time.time() - product_start_time
 
-i=0
-while i < N:
-    # Take the product of the DFA and the transition system to produce a graph of
-    # convex sets
-    print("Constructing GCS")
-    #start_point = [0.8, 0.1]
-    start_point = np.random.uniform(size=2)
-    order = 1
-    continuity = 0
-    product_start_time = time.time()
-    try:
-        bgcs = ts.Product(dfa, start_point, order, continuity)
-        product_time = time.time() - product_start_time
+# Solve the planning problem
+print("Solving Shortest Path")
+bgcs.AddLengthCost(norm="L2")
+solve_start_time = time.time()
+res = bgcs.SolveShortestPath(
+        convex_relaxation=True,
+        preprocessing=False,
+        verbose=True,
+        max_rounded_paths=1,
+        solver="mosek")
+solve_time = time.time() - solve_start_time
 
-        # Solve the planning problem
-        print("Solving Shortest Path")
-        bgcs.AddLengthCost(norm="L2")
-        #bgcs.AddDerivativeCost(norm="L2", degree=1, weight=0.5)
-        solve_start_time = time.time()
-        res = bgcs.SolveShortestPath(
-                convex_relaxation=True,
-                preprocessing=False,
-                verbose=False,
-                max_rounded_paths=1,
-                solver="mosek")
+if res.is_success():
+    # Plot the resulting trajectory
+    ts.visualize()
+    bgcs.PlotSolution(res, plot_control_points=True, plot_path=True)
+    plt.gca().xaxis.set_visible(False)
+    plt.gca().yaxis.set_visible(False)
+    
+    # Print timing infos
+    print("\n")
+    print("Solve Times:")
+    print("    LTL --> DFA    : ", dfa_time)
+    print("    TS x DFA = GCS : ", product_time)
+    print("    GCS solve      : ", solve_time)
+    print("    Total          : ", dfa_time + product_time + solve_time)
+    print("")
 
-        solve_time = time.time() - solve_start_time
-        print("Solve Time: ", solve_time)
-        solve_times.append(solve_time)
+    print("GCS vertices: ", bgcs.nv())
+    print("GCS edges: ", bgcs.ne())
 
-        i+= 1
-    except AssertionError:
-        # initial point was outside the workspace, so take a new sample
-        pass
+    plt.show()
+else:
+    print("Optimization failed!")
 
-print(repr(solve_times))
-
-#if res.is_success():
-#    # Plot the resulting trajectory
-#   # color_dict={"white":[[],["a"],["b"],["c"],["d"],["b","d"],["c","d"]]}
-#   # ts.visualize(color_dict, background='black', edgewidth=0.5, alpha=1.0)
-#    ts.visualize()
-#    bgcs.PlotSolution(res, plot_control_points=True, plot_path=True)
-#    plt.gca().xaxis.set_visible(False)
-#    plt.gca().yaxis.set_visible(False)
-#    
-#    # Print timing infos
-#    print("\n")
-#    print("Solve Times:")
-#    print("    LTL --> DFA    : ", dfa_time)
-#    print("    TS x DFA = GCS : ", product_time)
-#    print("    GCS solve      : ", solve_time)
-#    print("    Total          : ", dfa_time + product_time + solve_time)
-#    print("")
-#
-#    print("GCS vertices: ", bgcs.nv())
-#    print("GCS edges: ", bgcs.ne())
-#
-#    plt.show()
-#else:
-#    print("Optimization failed!")
-#
